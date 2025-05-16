5ed51132ef7c277866209fed44eabdeb81210228
Nobi58865 <168897920+Nobi58865@users.noreply.github.com>
2025-05-16 20:28:54 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,16 +3,16 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = "7699051363:AAFnmcRhUs15EQFpKOfj_1wewTN-IwTS2SI"
+BOT_TOKEN = ":7115851398:AAEtPzrkRGmbM8PZw7v3x6Qr5-cKdvBW-Bw"
 # From my.telegram.org/
-API_ID = "7880996"
-API_HASH =  "914be3dde138ba7e513eebaf88a83c14"
+API_ID = "24674133"
+API_HASH =  "a453ceef7b3df57e651afaf7367e6dd9"
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "6344607551").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("2088558960", "2088558960").split(" ")]
 # No time limit for this users
-AUTH_USERS = [6344607551, 6186862886]
+AUTH_USERS = [6344607551, 2088558960]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='testiverecordingbot' #bot username without@
+BOT_USERNAME='NobiGdrive_bot' #bot username without@
 API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
 
